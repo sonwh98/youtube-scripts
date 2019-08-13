931fa111822e7610c6a20e8a062b47eba58dae7c
sto <son.c.to@gmail.com>
2019-08-13 11:13:49 -0400

    init

---
+++ src/youtube_scripts/core.clj
@@ -0,0 +1,7 @@
+(ns youtube-scripts.core
+  (:require [etaoin.keys :as k])
+  (:use [etaoin.api]))
+
+
+
+(def driver (firefox))
---
+++ test/youtube_scripts/core_test.clj
@@ -0,0 +1,7 @@
+(ns youtube-scripts.core-test
+  (:require [clojure.test :refer :all]
+            [youtube-scripts.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
